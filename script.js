8274c09bee5eadbd22182040bd1a9d80f3fdc308
khanam9 <105882188+khanam9@users.noreply.github.com>
2024-06-19 15:53:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const watchNowButton = document.querySelector('.hero button');
+    watchNowButton.addEventListener('click', () => {
+        alert('Watch Now button clicked!');
+    });
+});
